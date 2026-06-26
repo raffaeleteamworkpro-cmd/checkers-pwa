4543c229f813ab72f1bdf04bad59a10707106344
raffaeleteamworkpro-cmd <raffaele.teamworkpro@gmail.com>
2026-06-26 18:53:33 +0200

    Update service-worker.js for layout and sharing improvements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'dama-v11';
+const CACHE = 'dama-v12';
 const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './gringo-ranch-hero.jpg'];
 
 self.addEventListener('install', event => {
